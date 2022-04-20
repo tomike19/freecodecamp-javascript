18e11f6291640e27d88b0ff907c32813f7b2688b
tomike19 <kolajoelizabeth@gmail.com>
2022-04-20 12:33:13 -0700

    word blank task

--- javaScript/word blanks.js
+++ javaScript/word blanks.js
@@ -4,5 +4,5 @@ const myVerb = "ran";
 const myAdverb = "quickly";
 
 // Only change code below this line
-const wordBlanks = "I have a" + "big" + " dog" +", that " + "ran" + "quickly" ; // Change this line
+const wordBlanks = "My " + myNoun + " is very " + myAdjective + " that he " + myVerb + " hello " + myAdverb + "." // Change this line
 // Only change code above this line
