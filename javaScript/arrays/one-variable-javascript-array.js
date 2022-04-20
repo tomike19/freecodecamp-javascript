d506d6692e87ab2ef930db2b8b5702a5bdf6c980
tomike19 <kolajoelizabeth@gmail.com>
2022-04-20 15:42:41 -0700

    Access Array Datas with indexes

--- javaScript/arrays/one-variable-javascript-array.js
+++ javaScript/arrays/one-variable-javascript-array.js
@@ -3,3 +3,10 @@ const myArray = ["hello", 12];
 
 //Nest one Array within Another Array
 const Array = [["sheeps", 23], ["rams", 45]];
+
+//Access Array Data with Indexes
+<p>Array indexes are written in the same bracket notation that strings use, except that instead of specifying a character, they are specifying an entry in the array. Like strings, arrays use zero-based indexing, so the first element in an array has an index of 0.</p>
+
+const myArrayIndex = [50, 60, 70];
+
+const myData=myArrayIndex[0];
