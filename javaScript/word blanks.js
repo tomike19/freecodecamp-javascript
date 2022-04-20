539a9cd48a7747acdd7f9e1c8bb28b06656ee572
tomike19 <kolajoelizabeth@gmail.com>
2022-04-20 11:26:16 -0700

    restructuring the folder structure

---
+++ javaScript/word blanks.js
@@ -0,0 +1,8 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "I have a" + "big" + " dog" +", that " + "ran" + "quickly" ; // Change this line
+// Only change code above this line
